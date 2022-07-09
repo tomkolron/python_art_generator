375262d292aa5bcff101c5b3842ba7e94f30efdd
mech-keyboard69420 <keyboard69420@protonmail.com>
2022-07-10 00:31:14 +0300

    made art generator a separate file and made styling better

--- www/script.js
+++ www/script.js
@@ -5,14 +5,15 @@ function gen() {
   var padding = $('.padding').val();
   var type = 2;
   var border_width = $('.border_width').val();
-  eel.gen_art(size, line_amount, line_width, padding, type, border_width)(function (ret) {
-    console.log(ret);
-    $('img').attr('src', ret);
-  })
+  for(let i = 0; i < 9; i++) {
+    eel.generate_art(size, line_amount, line_width, padding, type, border_width)(function (ret) {
+      $('.imgs_wrap').prepend('<img src="' + ret + '">');
+    })
+  }
 }
 
 $( document ).ready(function() {
   $('.size').on('input', function () {
-    $('.size_wrap > p').text($(this).val());
+    $('.size_label').text($(this).val());
   });
 });
