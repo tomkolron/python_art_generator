929f43f9ad910eb69d22f8e08089b0da528847e8
tomkolron <tomkolron@gmail.com>
2022-07-11 14:41:26 +0300

    changed port so it doesent interfear with docker

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def generate_art(size, amount, line_width, padding, type, border_width):
     return art_gen.gen_art(size, amount, line_width, padding, type, border_width)
 
 eel.init('www')
-eel.start('index.html')
+eel.start('index.html', port = 2000)
